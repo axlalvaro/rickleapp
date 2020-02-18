3808c40a2c428ca341913c11adebb3ee40dbb300
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2020-02-18 02:49:29 +0100

    Improved detail screen

--- src/Detail.js
+++ src/Detail.js
@@ -13,31 +13,36 @@ export default function Detail(props)
                         <div className="image" style={{ backgroundImage: `url(${props.character.image})` }}></div>
                         <h1>{ props.character.name }</h1>
                         <div className="description">
-                            <span className="green">
-                                { `${props.character.origin.name}, ` }
-                            </span>
+                            <span className="id">{ `#${props.character.id}` }</span>
                             { `${props.character.species} ${props.character.gender}` }
+                            { (props.character.type) ? `, ${props.character.type}` : null }
                         </div>
+                        <section className="origin">
+                            <div className="title">Origin</div>
+                            <div className="text green">{ props.character.origin.name }</div>
+                        </section>
                         <section className="lastLocation">
                             <div className="title">Last location</div>
-                            <div className="lastLocationText">{ props.character.location.name }</div>
+                            <div className="text">{ props.character.location.name }</div>
                         </section>
                         <section className="episodes">
                             <div className="title">Episodes</div>
 
-                            <div className="episodesContainer">
-                            {
-                                props.character.episode.map((episode, i) =>
+                            <Fade top cascade spy={props.character} duration={600}>
+                                <div className="episodesContainer">
                                 {
-                                    let episodeSplit = episode.split('/');
-                                    let episodeNum = episodeSplit[episodeSplit.length-1]
-                                    
-                                    return (
-                                        <div className="episode" key={i}>{ episodeNum }</div>
-                                    )
-                                })
-                            }
-                            </div>
+                                    props.character.episode.map((episode, i) =>
+                                    {
+                                        let episodeSplit = episode.split('/');
+                                        let episodeNum = episodeSplit[episodeSplit.length-1]
+                                        
+                                        return (
+                                            <div className="episode" key={i}>{ episodeNum }</div>
+                                        )
+                                    })
+                                }
+                                </div>
+                            </Fade>
                         </section>
                     </Fade>
                     <div className="giantName">{ props.character.name }</div>
